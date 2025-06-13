bf00a6081a1d9bd4b3d281eab2508a69a0d36a9b
am-ikharo <85495558+am-ikharo@users.noreply.github.com>
2025-06-13 23:54:28 +0100

    fixed a bug
    passed getImagesList wen calling the displayimages function instead of passing imagesList

--- main.js
+++ main.js
@@ -3,13 +3,11 @@ const slider = document.querySelector('.slider');
 
 async function fetchListOfImages() {
     try {
-        const response = await fetch('https://picsum.photos/v2/list?page=1&limit=5', {method: 'GET'});
-        
+        const response = await fetch('https://picsum.photos/v2/list?page=1&limit=10', {method: 'GET'});
         const imagesList = await response.json();
-        if(imagesList && imagesList.length > 0) displayImages()
-
-        console.log(imagesList);
-        
+        if(imagesList && imagesList.length > 0){
+            displayImages(imagesList)
+        }
     } catch (error) {
         console.log(error);
         
@@ -20,7 +18,7 @@ function displayImages(getImagesList){
     slider.innerHTML = getImagesList.map(
         (item) => `
         <div class='slider'>
-        <img src='${item.download_url} alt=${item.id}'/>
+        <img src=${item.download_url} alt=${item.id}/>
         </div>
         `).join(' ');
 }
