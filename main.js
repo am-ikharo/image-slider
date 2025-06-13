e9c41a3971f18601594b2a8ba9c65b7e1c11b2e3
am-ikharo <85495558+am-ikharo@users.noreply.github.com>
2025-06-13 23:34:52 +0100

    completed the asyn await function

--- main.js
+++ main.js
@@ -4,11 +4,23 @@ const slider = document.querySelector('.slider');
 async function fetchListOfImages() {
     try {
         const response = await fetch('https://picsum.photos/v2/list?page=1&limit=5', {method: 'GET'});
+        
+        const imagesList = await response.json();
+        if(imagesList && imagesList.length > 0) displayImages()
+
+        console.log(imagesList);
+        
     } catch (error) {
         console.log(error);
         
     }
 }
 
-
+function displayImages(getImagesList){
+    slider.innerHTML = getImagesList.map(item => `
+        <div class='slider'>
+        <img src='${item.download_url} alt=${item.}'/>
+        </div>
+        `)
+}
 fetchListOfImages();
