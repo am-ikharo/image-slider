4ecb29bb1d55084948be0d548b0443e8be40e042
am-ikharo <85495558+am-ikharo@users.noreply.github.com>
2025-06-14 00:03:55 +0100

    added the dots slider to the getimages function so d number of slide dotd will also b dynamic

--- main.js
+++ main.js
@@ -1,25 +1,38 @@
-const slider = document.querySelector('.slider');
-
+const slider = document.querySelector(".slider");
+const dotsContainer = document.querySelector(".dots-container");
 
 async function fetchListOfImages() {
-    try {
-        const response = await fetch('https://picsum.photos/v2/list?page=1&limit=10', {method: 'GET'});
-        const imagesList = await response.json();
-        if(imagesList && imagesList.length > 0){
-            displayImages(imagesList)
-        }
-    } catch (error) {
-        console.log(error);
-        
+  try {
+    const response = await fetch(
+      "https://picsum.photos/v2/list?page=1&limit=10",
+      { method: "GET" }
+    );
+    const imagesList = await response.json();
+    if (imagesList && imagesList.length > 0) {
+      displayImages(imagesList);
     }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-function displayImages(getImagesList){
-    slider.innerHTML = getImagesList.map(
-        (item) => `
+function displayImages(getImagesList) {
+  slider.innerHTML = getImagesList
+    .map(
+      (item) => `
         <div class='slider'>
         <img src=${item.download_url} alt=${item.id}/>
         </div>
-        `).join(' ');
+        `
+    )
+    .join(" ");
+
+  dotsContainer.innerHTML = getImagesList
+    .map(
+      (item, index) => `
+        <span class='dot' data-slide=${index}></span>
+        `
+    )
+    .join(" ");
 }
 fetchListOfImages();
