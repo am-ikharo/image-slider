d5ebfed70012e591ec64a0ea0b0a8a539e2c478e
am-ikharo <85495558+am-ikharo@users.noreply.github.com>
2025-06-13 23:19:59 +0100

    created asynawait method

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log('hello world');
+const slider = document.querySelector('.slider');
+
+
+async function fetchListOfImages() {
+    try {
+        const response = await fetch('https://picsum.photos/v2/list?page=1&limit=5', {method: 'GET'});
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+
+
+fetchListOfImages();
