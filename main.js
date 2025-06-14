d2da772a3e894ab3aa80a564fbc20b4f810d26c3
am-ikharo <85495558+am-ikharo@users.noreply.github.com>
2025-06-14 01:21:11 +0100

    created a change image function

--- main.js
+++ main.js
@@ -30,7 +30,9 @@ function displayImages(getImagesList) {
   dotsContainer.innerHTML = getImagesList
     .map(
       (item, index) => `
-        <span class='dot ${index === 0 ? 'active' : ''}' data-slide=${index}></span>
+        <span class='dot ${
+          index === 0 ? "active" : ""
+        }' data-slide=${index}></span>
         `
     )
     .join(" ");
@@ -39,31 +41,48 @@ fetchListOfImages();
 
 // slider functionality
 
-const slides = document.querySelectorAll('.slide');
-const prevBtn = document.querySelector('.prev');
-const nextBtn = document.querySelector('.next');
-let currentslide = 0;
+setTimeout(() => {
+  const slides = document.querySelectorAll(".slide");
+  const prevBtn = document.querySelector(".prev");
+  const nextBtn = document.querySelector(".next");
+  let currentslide = 0;
 
-function handleImageSlider(){
-    function activeDot(slide){
+  function activeDot(slide) {
+    document
+      .querySelectorAll(".dot")
+      .forEach((dotItem) => dotItem.classList.remove("active"));
+    document
+      .querySelector(`.dot[data-slide="${slide}"]`)
+      .classList.add("active");
+  }
 
+  function changeCurrentSlide(currentslide) {
+    slides.forEach(
+      (slideItem, index) =>
+        (slideItem.style.transform = `translateX(${
+          100 * (index - currentslide)
+        }%)`)
+    );
+  }
+
+  changeCurrentSlide(currentslide);
+
+  nextBtn.addEventListener("click", () => {
+    currentslide++;
+    if (slides.length - 1 < currentslide) {
+      currentslide = 0;
     }
-
-    function changeCurrentSlide(){
-
+    changeCurrentSlide(currentslide);
+    activeDot(currentslide);
+  });
+  prevBtn.addEventListener("click", () => {
+    currentslide--;
+    if (0 > currentslide) {
+      currentslide = slides.length;
     }
+    changeCurrentSlide(currentslide);
+    activeDot(currentslide);
+  });
 
-    nextBtn.addEventListener('click', () => {
-
-    })
-    prevBtn.addEventListener('click', () => {
-
-    })
-
-    dotsContainer.addEventListener('click', => {
-
-    })
-}
-
-
-handleImageSlider();
+  dotsContainer.addEventListener("click", () => {});
+}, 1000);
